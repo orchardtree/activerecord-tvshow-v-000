bd0844249ed0bd2c03ac02271684ead65f97ff5e
Jesse Wenger <wengerj@gmail.com>
2021-03-29 23:31:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,6 +3,6 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
   def self.most_popular_show
-    self.where("rating = ?", self.highest_rating)
+    self.where("rating = ?", self.maximum(:rating))
   end
 end
